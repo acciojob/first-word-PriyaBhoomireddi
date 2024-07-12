a0dca4d870672603d043974cbfb5ed8dbc15a366
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-07-12 14:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim leading and trailing whitespace
+  str = str.trim();
+  
+  // Find the index of the first space
+  const spaceIndex = str.indexOf(' ');
+  
+  // If there's no space, return the entire string
+  if (spaceIndex === -1) {
+    return str;
+  }
+  
+  // Otherwise, return the substring up to the first space
+  return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
-
+// Prompt user for input and display the result
 const s = prompt("Enter String:");
 alert(firstWord(s));
